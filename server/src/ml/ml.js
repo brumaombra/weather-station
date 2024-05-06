c3e4f9448dadeba6fdd6d61d70834eca0ca03c4c
mauro <maurobra97@gmail.com>
2024-05-06 12:09:38 +0200

    PM2.5 prediction

--- server/src/ml/ml.js
+++ server/src/ml/ml.js
@@ -1,10 +1,21 @@
 import * as tf from '@tensorflow/tfjs';
-import { getMeasurements } from '../db/sql.js';
 
-let modelPm25Prediction = null;
+// Normalize the data
+const normalizeData = (data, mean = null, std = null) => {
+    mean = mean || data.mean(0); // Calculate the mean if not provided
+    if (!std) { // Calculate the standard deviation if not provided
+        const squaredDiff = data.sub(mean).square();
+        std = squaredDiff.mean(0).sqrt();
+    }
+    return {
+        normalized: data.sub(mean).div(std),
+        mean: mean,
+        std: std
+    };
+};
 
-// Train the model
-const trainModel = async (X_train, y_train) => {
+// Train the model for regression
+export const trainModelRegression = async (X_train, y_train) => {
     try {
         if (!X_train || !y_train) throw new Error('Missing training data'); // Check the input values
         if (!Array.isArray(X_train) || !X_train.every(item => Array.isArray(item))) throw new Error('X_train must be an array of arrays');
@@ -14,34 +25,13 @@ const trainModel = async (X_train, y_train) => {
         const model = tf.sequential(); // Create a sequential model
         model.add(tf.layers.dense({ units: 64, activation: 'relu', inputShape: [X_train[0].length] }));
         model.add(tf.layers.dense({ units: 64, activation: 'relu' }));
-        model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
-        model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy', metrics: ['accuracy'] }); // Compile the model with the specified parameters
+        model.add(tf.layers.dense({ units: 1, activation: 'relu' }));
+        model.compile({ optimizer: 'adam', loss: 'meanSquaredError', metrics: ['accuracy'] }); // Compile the model with the specified parameters
         const xs = tf.tensor2d(X_train); // Convert the training data to a tensor
         const ys = tf.tensor2d(y_train, [y_train.length, 1]); // Convert the labels to a tensor
-        await model.fit(xs, ys, { epochs: 10 }); // Train the model for 10 epochs
-        return model; // Return the trained model
-    } catch (error) {
-        const newError = new Error('Error while getting the prediction', { cause: error }); // Save the old error to the stack
-        console.error(newError); // Log the error
-        throw newError; // Throw the error
-    }
-};
-
-// Train the PM2.5 model
-export const trainPm25Model = async () => {
-    try {
-        const measurements = await getMeasurements(); // Get the measurements from the database
-        const X_train = measurements.results.map(item => [ // Create the training data
-            item.temperature,
-            item.humidity,
-            item.pressure,
-            item.gas,
-            item.pm10
-        ]);
-        const y_train = measurements.results.map(item => item.pm25); // Create the labels
-        console.log('Training the model...');
-        modelPm25Prediction = await trainModel(X_train, y_train);
-        console.log('Model trained');
+        const { normalized: xs_normalized, mean: X_mean, std: X_std } = normalizeData(xs); // Normalize the data
+        await model.fit(xs_normalized, ys, { epochs: 10 }); // Train the model for 10 epochs
+        return { model, X_mean, X_std }; // Return the trained model and the normalization parameters
     } catch (error) {
         const newError = new Error('Error while training the model', { cause: error }); // Save the old error to the stack
         console.error(newError); // Log the error
@@ -49,12 +39,14 @@ export const trainPm25Model = async () => {
     }
 };
 
-// Get the PM2.5 prediction
-export const getPm25Prediction = async X_test => {
+// Get the prediction
+export const getPrediction = async (model, X_test, X_mean, X_std) => {
     try {
-        const xs = tf.tensor2d(X_test); // Convert the data to a tensor
-        const prediction = modelPm25Prediction.predict(xs); // Make a prediction
-        return prediction.dataSync(); // Return the prediction
+        const xs = tf.tensor2d(X_test); // Convert the test data to a tensor
+        const { normalized: xs_normalized } = normalizeData(xs, X_mean, X_std); // Normalize the test data
+        const prediction = await model.predict(xs_normalized); // Make a prediction
+        const result = await prediction.dataSync(); // Get the prediction result
+        return result;
     } catch (error) {
         const newError = new Error('Error while getting the prediction', { cause: error }); // Save the old error to the stack
         console.error(newError); // Log the error
