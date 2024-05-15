fd7f8b2b451da11e08be04941689f95993a207ce
mauro <maurobra97@gmail.com>
2024-05-15 17:28:55 +0200

    New UI

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css';
 import router from './router';
 import { createApp } from 'vue';
 import App from './App.vue';
+import "preline/preline";
 
 const app = createApp(App);
 app.use(router);
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,13 +1,14 @@
-import { createRouter } from 'vue-router';
-import { createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 import Measurements from '@/views/Measurements.vue';
 import Charts from '@/views/Charts.vue';
 import Forecasts from '@/views/Forecasts.vue';
 import Correlations from '@/views/Correlations.vue';
 import Login from '@/views/Login.vue';
 import NotFound from '@/views/NotFound.vue';
+import Dashboard from '@/views/dashboard/Dashboard.vue';
 
-export default createRouter({
+// Router
+const router = createRouter({
     history: createWebHistory('/'),
     routes: [{
         path: '/',
@@ -30,8 +31,36 @@ export default createRouter({
         name: 'login',
         component: Login
     }, {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Dashboard,
+        children: [{
+            path: 'measurements',
+            name: 'measurements',
+            component: Measurements
+        }, {
+            path: 'charts',
+            name: 'charts',
+            component: Charts
+        }, {
+            path: 'correlations',
+            name: 'correlations',
+            component: Correlations
+        }]
+    }, {
         path: '/:pathMatch(.*)*',
         name: 'notFound',
         component: NotFound
     }]
 });
+
+// Setup preline
+router.afterEach((to, from, failure) => {
+    if (!failure) {
+        setTimeout(() => {
+            window.HSStaticMethods.autoInit();
+        }, 100);
+    }
+});
+
+export default router;
