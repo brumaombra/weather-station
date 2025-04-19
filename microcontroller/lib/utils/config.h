6faa356c6bff902a9af50b06b54dc486e353e705
Mauro Brambilla <maurobra97@gmail.com>
2025-04-19 17:26:58 +0200

    Clear Wi-Fi and server settings in config.h for security and privacy

--- microcontroller/lib/utils/config.h
+++ microcontroller/lib/utils/config.h
@@ -7,12 +7,12 @@
 #define DEV_MODE true
 
 // WiFi settings
-#define WIFI_SSID "EOLO-Bettola"
-#define WIFI_PASSWORD "jfJ8c24LM"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
 // Server settings
-#define SERVER_ENDPOINT "http://192.168.21.10:3000/api/measurements"
-#define AUTH_TOKEN "0123456789"
+#define SERVER_ENDPOINT ""
+#define AUTH_TOKEN ""
 
 // Device settings
 #define READING_INTERVAL 60 * 15 // Reading interval in seconds
